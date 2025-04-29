66de7e263f3896191ee3d531c19c170ae6920b37
BLacey99 <Blake.M.Lacey@gmail.com>
2025-04-29 14:13:06 -0400

    DB setup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+
+dotenv.config();
+
+const app = express();
+
+console.log(process.env.MONGO_URI);
+
+app.get("/", (req, res) =>{
+    res.send("Server is ready")
+});
+
+app.get("/products", (req, res)=>{
+    res.send("Products")
+});
+
+app.get("/cart", (req, res)=>{
+    res.send("Cart")
+});
+
+app.listen(5000, ()=> {
+    connectDB();
+    console.log("Server started at http://localhost:5000")
+});
